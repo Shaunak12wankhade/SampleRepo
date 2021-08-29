9f823a117d1de2fe07b60aadfd3aa0e1cfa04e9c
unknown <shaunak12wankhade@ggmail.com>
2021-08-29 10:20:28 +0530

    Adding NewProject

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp059
+{
+    class Car
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving");
+        }
+        public static void Brake()
+        {
+            Console.WriteLine("Brake");
+        }
+        static void main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.Drive();
+            Brake();
+            Console.Read();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp059
+{
+    // access modifier class className
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.Drive();
+            Car.Brake();
+        }
+    }
+}
